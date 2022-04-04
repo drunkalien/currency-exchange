a35797a9b3e79901939db6ccffc425558d57e5e9
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-04-04 11:10:30 +0500

    some changes

--- src/services/api/request.ts
+++ src/services/api/request.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 export const requestInstance = axios.create({
-  // baseURL: "http://data.fixer.io/api/",
   baseURL: "https://api.exchangerate.host/",
   params: {
-    // access_key: "cdce3b38c10ceb3fa5fdf188221099c3",
     places: 2,
   },
 });
